bd5ad908d79b8c333879878689a1563a9d5b0ad7
Raruben <rrubenstein@brandeis.edu>
2018-11-10 13:14:06 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class Main{
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         System.out.println("The Emperor Protects.");
+        System.out.println("Welcome to MadLibs!");
     }
 }
