cc9dea041f22d2d89b2bbf6569ae5a9e55ea4df0
Raruben <rrubenstein@brandeis.edu>
2018-11-10 13:54:49 -0500

    Update MadLibs.java

---
+++ MadLibs.java
@@ -0,0 +1,16 @@
+public class MadLibs {
+
+  public static void main(String[] args) {
+
+    int rand = (int)(8*Math.random());
+    System.out.println(rand);
+    switch(rand) {
+
+      case = 0; Hanukkah(); break;
+      case = 1; Christmas(); break;
+      case = 2; Thanksgiving(); break;
+      case = 3; NewYears(); break;
+
+    }
+  }
+}
