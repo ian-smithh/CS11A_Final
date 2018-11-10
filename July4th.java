df042e7e461e2e81db853816074e339e207158ca
Sarah <sarahfielman@brandeis.edu>
2018-11-10 14:55:10 -0500

    July4th.java

---
+++ July4th.java
@@ -0,0 +1,6 @@
+public class July4th{
+ public static void main(String[] args){
+ String july = "Every July 4th my family and I [verb] with a picnic at the [place]. \nThe [place] is [adverb] [adjective], with the tall, lafy [plural noun], [animals] [verb +ing] and a [verb +ing] trail. \nWe [verb] lots of [adjective] food and drinks, like [food], [food], and [beverage]. \nMy parents like [verb] [food] over a hot [noun] and we put mustard and [plural noun] on top. \nAfter we eat, we [verb] games, like [verb] the [noun], [verb], and [adjective] [noun][verb].\n";
+ System.out.print(july);
+ }
+}
