4ea818dbc8d34ddba56c7bb1a21fedf4dc79c082
Raruben <rrubenstein@brandeis.edu>
2018-11-10 14:26:48 -0500

    Update NewYears.java

--- NewYears.java
+++ NewYears.java
@@ -1,8 +1,6 @@
 public class NewYears {
-
-  String ny = "1. I will [verb] a race to raise [noun] for a charity./n
-  2. I will learn to [verb] a new language. /n
-  3. I will [adverb] try to improve my [plural noun] in Math./n
-  4. I will volunteer to [verb] [plural noun] in our local [noun]./n
-  5. After school, I will"
+  public static void main(String[] args) {
+    String ny = "1. I will [verb] a race to raise [noun] for a charity.\n2. I will learn to [verb] a new language. \n3. I will [adverb] try to improve my [plural noun] in Math. \n4. I will volunteer to [verb] [plural noun] in our local [noun]. \n5. After school, I will [verb] for [number] minutes every night to improve my reading. \n6. At home, I will [verb] my parents with [verb] dinner and [verb] the dishes afterwards. \n7. I will limit my computer time to [adjective] minutes a day. \n8. I will do my best to do what my [plural noun] ask of me... the [number] time they ask. \n9. I will eat [number] fruits and [plural noun] every day. \n10. I will follow The [adjective] Rule: Always treat [plural noun] the way that you want to be [verb].";
+    System.out.print(ny);
+}
 }
