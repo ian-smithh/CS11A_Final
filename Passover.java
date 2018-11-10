d39d69e2327c7c12ab5f35cff0f65e87ff6c3a50
Sarah <sarahfielman@brandeis.edu>
2018-11-10 15:15:20 -0500

    Passover

---
+++ Passover.java
@@ -0,0 +1,6 @@
+public class Passover{
+  public static void main(String[] args){
+  String passover = "Every year we celebrate Passover to remeber when our ancesters were \n[occupation, plural] and how they [past tense verb] to freedom. \nMoses came to Pharaoh and said, 'Let me people [verb]!' but the Pharaoh refused. \nThis made G-d [adjective], so he unleased [number] plauges to change the Pharaoh's [noun]. \nFirst, he turned all the water into [liquid]. That did not work. So G-d tried other plauges like [plural noun], \nstorms, and darkness. [number] plagues later, the Pharaoh still wouldn't let the [plural noun] go. \nThen came the [adjective] plague: one morning all the [adjective] Egyptian children suddenly died. \nThe Pharaoh finally gave up and agreed to [verb] the Jews.\n";
+  System.out.print(passover);
+}
+}
