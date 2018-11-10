8da88ecae5b271c144d75237001eb135e6247c4a
Sarah <sarahfielman@brandeis.edu>
2018-11-10 14:35:42 -0500

    Thanksgiving

---
+++ Thanksgiving.java
@@ -0,0 +1,27 @@
+public class Thanksgiving{
+  public static void main(String[] args){
+
+String tg = "The first Thanksgiving was held in [year] when the pilgrims and \nIndians came together for [adjective] meal to celebrate their first \n[noun] in America. It was [adjective]! The Indians \nbrought [food] to the festivities, and the pilgrims \n[verb, past tense] [plural noun] for everyone. The feast lasted for [number] days! \nIn [year], President [famous person] declared the 4th Thursday of \n[month] as a day of [plural noun] and [plural noun] and called it Thanksgiving. \nModern Thanksgiving celebrating usually include cooking a large \n[noun] with stuffing and [noun] sauce, eating [noun] \npie, and watching [noun] games on television. My favorite thing \nabout Thanksgiving is the [adjective] [food] -- what about you?\n";
+System.out.print(tg);
+  }
+}
+
+/*System.out.println("Enter 2 years, i.e 1987");
+String year = TextIO.getlnWord()
+System.out.println("Enter 3 adjectives");
+String adjective = TextIO.getlnWord()
+System.out.println("Enter 4 nouns");
+String noun = TextIO.getlnWord()
+System.out.println("Enter 3 plural nouns");
+String pnoun = TextIO.getlnWord()
+System.out.println("Enter a past tense verb");
+String ptverb = TextIO.getlnWord()
+System.out.println("Enter 2 foods");
+String food = TextIO.getlnWord()
+System.out.println("Enter a number");
+String number = TextIO.getlnWord()
+System.out.println("Enter a month");
+String month = TextIO.getlnWord()
+System.out.println("Enter the name of a famous person");
+String person = TextIO.getlnWord()
+*/
