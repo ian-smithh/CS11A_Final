86580e0c43fff26703d8d30aa623794faa3009a7
Raruben <rrubenstein@brandeis.edu>
2018-11-10 14:46:56 -0500

    Update Halloween.java

--- Halloween.java
+++ Halloween.java
@@ -3,6 +3,7 @@ public class Halloween {
   public static void main(String[] args) {
 
     String hw = "My school is pretty [adjective] for most of the year, except in late October, [adjective] cobwebs appear in the hallway, with really [adjective] [animals] hanging from them. The lunch room has orange and black [nouns] everywhere, and they serve roasted [noun] for lunch. Someone told me that a giant [adjective] [animal] took over the principal's office. All of the teachers look different; one is a zombie with [color] hair, another is a [adjective] [noun], and I think my [noun] teacher is a [noun] now. Tombstones line the hallways, and one said 'Here lies [name]', who died of [verb].";
+    
     System.out.println(hw);
   }
 }
