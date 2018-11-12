26fe3b392bcebb9bf640da4242a246f032bc456e
Raruben <rrubenstein@brandeis.edu>
2018-11-12 10:49:03 -0500

    Update Thanksgiving.java
    Update method

--- Thanksgiving.java
+++ Thanksgiving.java
@@ -3,28 +3,27 @@ public class Thanksgiving{
 
     String tg = "The first Thanksgiving was held in [year] when the pilgrims and \nIndians came together for [adjective] meal to celebrate their first \n[nouns] in America. It was [adjective]! The Indians \nbrought [food] to the festivities, and the pilgrims \n[verb, past tense] [pluraln] for everyone. The feast lasted for [number] days! \nIn [year], President [famous person] declared the 4th Thursday of \n[month] as a day of [pluraln] and [pluraln] and called it Thanksgiving. \nModern Thanksgiving celebrating usually include cooking a large \n[nouns] with stuffing and [nouns] sauce, eating [nouns] \npie, and watching [nouns] games on television. My favorite thing \nabout Thanksgiving is the [adjective] [food] -- what about you?\n";
     System.out.print(tg);
-    int num_noun = Noun(tg);
-    String[] mlnouns = new String[num_noun];
-    System.out.println(num_noun);
-    for(int i=0; i<num_noun; i++) {
+    Noun(tg);
+    Adjective(tg);
+    PluralN(tg);
+    Year(tg);
+    FamousPerson(tg);
+  }
+
+
+  public static void Noun(String ml) {
+    int count=0;
+    count = ml.split("\\[nouns\\]").length-1;
+    String[] mlnouns = new String[count];
+    for(int i=0; i<count; i++) {
       System.out.print("Enter a noun: ");
       mlnouns[i] = TextIO2.getlnWord();
     }
-    for(int i=0; i<num_noun; i++) {
+    for(int i=0; i<count; i++) {
       System.out.println (mlnouns[i]);
-
     }
-
-
-  }
-
-  public static int Noun(String ml) {
-    int count=0;
-    count = ml.split("\\[nouns\\]").length-1;
-    return count;
   }
 }
-
 /*System.out.println("Enter 2 years, i.e 1987");
 String year = TextIO.getlnWord()
 System.out.println("Enter 3 adjectives");
