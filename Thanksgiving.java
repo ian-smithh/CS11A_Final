360fc623fd1af24d36dc52a5788744d6bcb1d534
Raruben <rrubenstein@brandeis.edu>
2018-11-12 10:41:19 -0500

    Update Thanksgiving.java
    Method to find nouns

--- Thanksgiving.java
+++ Thanksgiving.java
@@ -1,16 +1,17 @@
 public class Thanksgiving{
   public static void main(String[] args){
 
-    String tg = "The first Thanksgiving was held in [year] when the pilgrims and \nIndians came together for [adjective] meal to celebrate their first \n[noun] in America. It was [adjective]! The Indians \nbrought [food] to the festivities, and the pilgrims \n[verb, past tense] [plural noun] for everyone. The feast lasted for [number] days! \nIn [year], President [famous person] declared the 4th Thursday of \n[month] as a day of [plural noun] and [plural noun] and called it Thanksgiving. \nModern Thanksgiving celebrating usually include cooking a large \n[noun] with stuffing and [noun] sauce, eating [noun] \npie, and watching [noun] games on television. My favorite thing \nabout Thanksgiving is the [adjective] [food] -- what about you?\n";
+    String tg = "The first Thanksgiving was held in [year] when the pilgrims and \nIndians came together for [adjective] meal to celebrate their first \n[nouns] in America. It was [adjective]! The Indians \nbrought [food] to the festivities, and the pilgrims \n[verb, past tense] [pluraln] for everyone. The feast lasted for [number] days! \nIn [year], President [famous person] declared the 4th Thursday of \n[month] as a day of [pluraln] and [pluraln] and called it Thanksgiving. \nModern Thanksgiving celebrating usually include cooking a large \n[nouns] with stuffing and [nouns] sauce, eating [nouns] \npie, and watching [nouns] games on television. My favorite thing \nabout Thanksgiving is the [adjective] [food] -- what about you?\n";
     System.out.print(tg);
     int num_noun = Noun(tg);
     String[] mlnouns = new String[num_noun];
+    System.out.println(num_noun);
     for(int i=0; i<num_noun; i++) {
       System.out.print("Enter a noun: ");
       mlnouns[i] = TextIO2.getlnWord();
     }
     for(int i=0; i<num_noun; i++) {
-      System.out.print(mlnouns[i]);
+      System.out.println (mlnouns[i]);
 
     }
 
@@ -18,14 +19,8 @@ public class Thanksgiving{
   }
 
   public static int Noun(String ml) {
-    String[] nouns = new String[ml.length()];
-    for(int i)
     int count=0;
-    for(int i=0; i<nouns.length; i++) {
-      if(nouns[i].equals("[")) {
-        count++;
-      }
-    }
+    count = ml.split("\\[nouns\\]").length-1;
     return count;
   }
 }
