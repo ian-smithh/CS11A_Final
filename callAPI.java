897d494761b26cdf9b1a23f7b9c7bf1f1b0c491b
Ian Smith <44270052+ian-smithh@users.noreply.github.com>
2018-11-26 14:04:49 -0500

    Started development on a new way to use the API.

--- callAPI.java
+++ callAPI.java
@@ -4,45 +4,34 @@ import java.util.*;
 import java.math.*;
 public class callAPI{
     public static void main(String[] args) throws Exception{
-        double min = (Math.random() * 100);
-        double max = (Math.random() * 100);
-        if (max < min){
-            double temp = min;
-            min = max;
-            max = temp;
-        }
-        URL api = new URL("http://madlibz.herokuapp.com/api/random?minlength="+min+"&maxlength="+max);
+        URL api = new URL("http://madlibz.herokuapp.com/api/random?minlength=10&maxlength=25");
         BufferedReader in = new BufferedReader(new InputStreamReader(api.openStream()));
-        BufferedWriter writer = new BufferedWriter(new FileWriter("apiOut.txt"));
-        PrintWriter formatter = new PrintWriter(writer);
+        //BufferedWriter writer = new BufferedWriter(new FileWriter("apiOut.txt"));
+        //PrintWriter formatter = new PrintWriter(writer);
         String line = "";
-
+        String apiStore [] = new String[60]; 
+        int counter = 0;
         while ((line = in.readLine())!= null){
-            //System.out.println(line);
-            formatter.printf("%s %n",line);
+            apiStore[counter] = line;
+            counter +=1;
         }
         in.close();
-        writer.close();
-        formatter.close();
-        getInput("apiOut.txt");
+        //writer.close();
+        //formatter.close();
+        //getInput(apiStore);
+        for(int i = 0;i < apiStore.length;i+=1){
+            System.out.println(apiStore[i].toString());
+        }
     }
 
-    public static void getInput(String fileName) throws FileNotFoundException{
+    public static void getInput(String array[]){
         //Read the "blanks" section and output
-        Scanner readFile = new Scanner(fileName);
-        while(readFile.hasNext()){
-            if(readFile.next().equals("]")){
-                break;
-            }
-            else{
-                readFile.next();
-            }
-        }
+        //for(int i = 0;)
         //Store input in an array
         //Print "value" section with inputs from array
         //???
         //Profit
-        readFile.close();
+        //readFile.close();
     }
     
 }
