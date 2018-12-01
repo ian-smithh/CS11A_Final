97d4993c2b73f601be202b9e05c30327517ed5f1
Ian Smith <44270052+ian-smithh@users.noreply.github.com>
2018-11-30 22:35:17 -0500

    Updated Halloween with descriptive statement

--- Halloween.java
+++ Halloween.java
@@ -12,7 +12,7 @@ public class Halloween {
    */
 
   public static void main(String[] args) {
-
+    System.out.println("This is a Madlib about Halloween");
     String hw = "My school is pretty [adjective] for most of the year, except in late October, [adjective] cobwebs appear in the hallway, with really [adjective] [animals] hanging from them. The lunch room has orange and black [plural] everywhere, and they serve roasted [noun] for lunch. Someone told me that a giant [adjective] [animal] took over the principal's office. All of the teachers look different; one is a zombie with [color] hair, another is a [adjective] [noun], and I think my [noun] teacher is a [noun] now. Tombstones line the hallways, and one said 'Here lies [name]', who died of [verb].";
     String result = Noun(hw);
     result = Plural(result);
