9cf63f1c6c3072184172a34c6d9f71b375f5f1a0
Ian Smith <44270052+ian-smithh@users.noreply.github.com>
2018-12-03 12:57:41 -0500

    Added javadoc to main

--- main.java
+++ main.java
@@ -2,6 +2,10 @@
  * Introduces the user to the program and calls the MadLibs method.
  */
 public class main{
+    /**
+     * Main introduces the program to the user and runs a loop to continue running the MadLibs class until the user stops giving consent
+     * @param args Main does not accept input from args
+     */
     public static void main(String[] args) {
         System.out.println("Pray to RNJesus for a good roll!");
         MadLibs.main(args);
