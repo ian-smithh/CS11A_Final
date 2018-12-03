54d133e510235c0dcae296a1ad2bbd9497f2aa50
Ian Smith <44270052+ian-smithh@users.noreply.github.com>
2018-12-03 12:52:50 -0500

    Added a loop to allow the main to run forever. Resolves issue #8

--- main.java
+++ main.java
@@ -5,5 +5,11 @@ public class main{
     public static void main(String[] args) {
         System.out.println("Pray to RNJesus for a good roll!");
         MadLibs.main(args);
+        boolean consent = true;
+        while(consent){
+            System.out.println("Another?");
+            consent = TextIO.getBoolean();
+            MadLibs.main(args);
+        }
     }
 }
